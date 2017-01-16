28b9dfcc99bd7de8e5bfc36ebfaec46ea1b86abb
siyammahmud <sayemchowdhury21@gmail.com>
2017-01-16 22:54:28 +0600

    Add files via upload

---
+++ js/myscript.js
@@ -0,0 +1,12 @@
+$(document).ready(function(){
+
+	$("button").click(function(){
+
+		$("p").css({
+			"overflow" : "scroll",
+			"min-height" : "150px",
+			"max-height" : "150px"
+
+		});
+	});
+});
